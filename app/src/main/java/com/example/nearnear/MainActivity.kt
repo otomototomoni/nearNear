f65f6b98cda0e227f703fb25d93cb05585766e70
otomototomoni <tegurechafu150415@icloud.com>
2025-02-14 14:27:48 +0900

    initial commit

--- app/src/main/java/com/example/nearnear/MainActivity.kt
+++ app/src/main/java/com/example/nearnear/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             NearNearTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "kotlin",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
