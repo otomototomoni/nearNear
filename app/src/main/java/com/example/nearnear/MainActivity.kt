c5d5c56e906ae34d693350dcf05a6cffe1280283
otomototomoni <tegurechafu150415@icloud.com>
2025-02-18 03:02:32 +0900

    パーミッションの許可を促すダイアログを表示することに成功。
    ToDo:
    パーミッションについて→
    権限を許可したときに画面遷移を行えるようにする。
    権限の許可の仕方によってエラーを吐いてしまう。ユーザーの操作に応じて処理をする必要がある。
    MainActivityで直接処理を書いているためできれば他のファイルでコードを記述したい。
    現在地の取得について→
    過去に取得した現在地しか取得できない状態のため新規で現在地を取得できるようにする。

--- app/src/main/java/com/example/nearnear/MainActivity.kt
+++ app/src/main/java/com/example/nearnear/MainActivity.kt
@@ -1,22 +1,71 @@
 package com.example.nearnear
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
+import com.example.nearnear.Gps.checkLocationPermission
 import com.example.nearnear.ui.theme.NearNearTheme
 
 /*
- *  AppNavHost.ktのAppNavHost()を実行して画面遷移の処理をする
+ * AppNavHost.ktのAppNavHost()を実行して画面遷移の処理をする
+ * GPSのパーミッションの権限をチェックする。権限が無効の場合、ユーザーに権限を要求する
  */
 class MainActivity : ComponentActivity() {
+
+    //取得する権限の配列を準備する
+    private val REQUIRED_PERMISSIONS =
+        mutableListOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ).toTypedArray()
+    //リクエストコード
+    private val REQUEST_CODE_PERMISSIONS = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NearNearTheme {
-                AppNavHost()
             }
+
+            //GPSのパーミッションの権限チェック
+                if (checkLocationPermission(this)) {
+                    //すでに権限がある場合の処理
+                    AppNavHost()
+                }else {
+                    //権限がない場合の処理（ユーザーに権限を要求）& ToDo:パーミッションが必要な理由を説明するUIを表示
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION)){
+                        //ユーザーに権限を要求する
+                        ActivityCompat.requestPermissions(
+                            this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                        )
+                    }else {
+                        //パーミッションをリクエストする
+                        ActivityCompat.requestPermissions(
+                            this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+                        )
+                    }
+                }
         }
     }
+
+    //確認ダイアログの結果を受け取る
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //再度権限をチェックする
+        if (checkLocationPermission(this)) {
+            //確認ダイアログで権限を取得した場合の処理
+        } else {
+            //権限を取得できなかった場合の処理
+            finish()
+        }
+    }
+
 }
